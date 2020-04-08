0c4e1641f37310549f56694e36541e583d147581
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-04-08 10:57:15 +0530

    ADDED: Ensure player to get exact winning position

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -13,23 +13,33 @@ currentPosition=0
 
 #Execution start from here
 
-while [[ $currentPosition -ne $END_POSITION ]]
+while [[ $currentPosition -le $END_POSITION ]]
 do
    rollToPlay=$((RANDOM%3))
 
 case $rollToPlay in
 	$PLAY)
 		dieRoll=$(($((RANDOM%6))+1))
-		currentPosition=$((currentPosition+dieRoll))
-		;;
 
+		if [[ $((END_POSITION-currentPosition)) -eq $dieRoll ]]
+		then
+			currentPosition=$((currentPosition+dieRoll))
+
+		elif [[ $((END_POSITION-currentPosition)) < $randomPosition ]]
+		then
+			currentPosition=$currentPosition
+		else
+			currentPosition=$((currentPosition+dieRoll))
+		fi
+		;;
 	$SNAKE)
 		dieRoll=$(($((RANDOM%6))+1))
 		if [[ $currentPosition -eq 0 || $currentPosition -lt $dieRoll ]]
-			then
-				currentPosition=$currentPosition;
+		then
+			currentPosition=$currentPosition
+
 		else
-				currentPosition=$((currentPosition-dieroll))
+			currentPosition=$((currentPosition-dieroll))
 		fi
 		;;
 	$NOPLAY)
