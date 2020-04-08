2e216aa37f710794bdaa57f8f548959e7e9cb070
sanjujaiswal <sanjayjaiswal130082@gmail.com>
2020-04-08 09:51:04 +0530

    ADDED: Check position Play,NoPlay or Snake bite

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -4,9 +4,29 @@ echo "---Welcome to Snake Ladder Game---"
 #Constants declaration
 START_POSITION=0
 END_POSITION=100
+PLAY=0
+SNAKE=1
+NOPLAY=2
 
 #Variables declaration
 currentPosition=0
 
-#Rolling the die ton get number between  1 to 6.
-dieRoll=$((RANDOM%6+1))
+#Execution stert from here
+
+rollToPlay=$((RANDOM%3))
+
+case $rollToPlay in
+$PLAY)
+	dieRoll=$(($((RANDOM%6))+1))
+	currentPosition=$((currentPosition+dieRoll))
+	;;
+
+$SNAKE)
+	dieRoll=$(($((RANDOM%6))+1))
+	currentPosition=$((currentPosition-dieroll))
+	;;
+
+$NOPLAY)
+	currentPosition=$((currentPosition+0))
+	;;
+esac
